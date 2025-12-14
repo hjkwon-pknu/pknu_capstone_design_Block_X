e6316c3c3e838253550cc0d24884b288506b6ad3
jinn <121280206+hjkwon-pknu@users.noreply.github.com>
2025-12-14 12:40:23 +0900

    Update ml_step1_baseline.py

--- BTC-Trading-System-Capstone/experiments/ml_step1_baseline.py
+++ BTC-Trading-System-Capstone/experiments/ml_step1_baseline.py
@@ -36,8 +36,11 @@ except:
   from sklearn.linear_model import LogisticRegression
   from sklearn.metrics import accuracy_score, balanced_accuracy_score, classification_report
 
-FILE_PATH = r"C:\Users\User\binance_data\1m_history.csv"  # 필요 시 경로 수정
-USE_ROWS = 1_000_000  # 처음엔 50만~100만 행으로 시작
+#################################
+import os
+FILE_PATH = os.environ.get("BTC_FILE_PATH", r"C:\Users\User\binance_data\1m_history.csv")
+USE_ROWS = None
+#################################
 
 def load_data(path, use_rows=None):
   df = pd.read_csv(path, usecols=['datetime','open','high','low','close','volume'])
